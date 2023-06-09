9a3ede7e6327e01775f5cdae1f4125ebeb8a3a7a
Tanya <tanya@example.com>
2023-06-09 04:22:10 +0300

    exercise 2

---
+++ src/main/java/org/example/Lesson1/Ex2.java
@@ -0,0 +1,30 @@
+package org.example.Lesson1;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+//         "1 2 1 2 -1 1 3 1 3 -1 0";
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println("exercise2(n, scan) = " + exercise2(n, scan));
+    }
+
+    private static int exercise2(int n, Scanner scan) {
+
+        int result = 0;
+        int a = 0, b = 0;
+        a = scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            b = scan.nextInt();
+            if (a > 0 && b < 0);
+            result = i++;
+            a = b;
+        }
+        scan.close();
+        return result;
+    }
+}
+
+
