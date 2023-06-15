d9339c96ef5c59b4db88558180e6748c6d50fd7a
Tanya <tanya@example.com>
2023-06-15 11:58:18 +0300

    exercise 2

--- src/main/java/org/example/Lesson2/ex2.java
+++ src/main/java/org/example/Lesson2/ex2.java
@@ -1,2 +1,26 @@
-package org.example.Lesson2;public class ex2 {
+package org.example.Lesson2;
+
+import java.util.Arrays;
+
+public class ex2 {
+    public static void main(String[] args) {
+        int[] n = new int[10];
+        for (int i = 0; i < n.length; i++) {
+            n[i] = (int) (Math.random() * 10);
+        }
+        System.out.println(Arrays.toString(n));
+        boolean flag = true;
+        for (int i = 1; i < n.length; i++) {
+            if (n[i] <= n[i - 1]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag){
+            System.out.println("Последовательность возрастающая");
+        }
+        else {
+            System.out.println("Последовательность убывающая");
+        }
+    }
 }
