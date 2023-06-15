24a254e6b3785697d465bcc5688b52ed5a670334
Tanya <tanya@example.com>
2023-06-15 11:58:31 +0300

    exercise 3

--- src/main/java/org/example/Lesson2/ex3.java
+++ src/main/java/org/example/Lesson2/ex3.java
@@ -1,2 +1,23 @@
-package org.example.Lesson2;public class ex3 {
+package org.example.Lesson2;
+import java.util.Arrays;
+
+public class ex3 {
+    public static void main(String[] args) {
+        int[] n= new int[]{1, 22, 33, 44, 5, -5, -3, 23, -7};
+        System.out.println(Arrays.toString(n));
+        int tmp = 0;
+        for (int i = 0; i < n.length; i++) {
+            if(n[i] > 9){
+                tmp +=i;
+            }
+        }
+        System.out.println("Сумма двузначных элементов массива = " + tmp);
+//        int [] n1 = new int[];
+//        int[]n1 = int[]n;
+        for (int i = 0; i <n.length ; i++) {
+            if (n[i]<0) n[i] = tmp;
+        }
+        System.out.println(Arrays.toString(n));
+    }
 }
+
