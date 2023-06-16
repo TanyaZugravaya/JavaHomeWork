715e0a6c41c5223cf8f67b838ce3d9605febfd66
Tanya <tanya@example.com>
2023-06-16 22:36:07 +0300

    exercise 2

--- src/main/java/org/example/Lesson3/Ex2/Main.java
+++ src/main/java/org/example/Lesson3/Ex2/Main.java
@@ -6,39 +6,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Product product1 = new Product("древесина1", "США", 200, 1700, 2);
+        Product product2 = new Product("древесина2", "Индия", 300, 3000, 1);
+        Product product3 = new Product("древесина3", "Канада", 200, 1000, 3);
+        Product product4 = new Product("древесина4", "Россия", 100, 1000, 3);
+        Product product5 = new Product("древесина5", "Китай", 200, 2500, 1);
+        List<Product> woodList = new ArrayList<>();
+        woodList.add(product1);
+        woodList.add(product2);
+        woodList.add(product3);
+        woodList.add(product4);
+        woodList.add(product5);
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        List<Product> woodList = generateCollection(n);
-
-//        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сорт товара: ");
-        String searchItem = scanner.nextLine();
-
-        for (int i = 0; i < woodList.size(); i++) {
-
-        }
+        int sort = scanner.nextInt();
+        System.out.printf("searchNameProduct = " + searchNameProduct(sort, woodList));
     }
 
-    private static ArrayList<Product> generateCollection(int n, Scanner scanner) {
-//        Product product = new Product("древесина", "США", 200, 1000, 2 );
-//        Product product2 = new Product("древесина", "Индия", 300, 3000, 1);
-//        Product product3 = new Product("древесина", "Канада", 200, 1700, 3 );
-//        Product product4 = new Product("древесина", "Россия", 100, 1000, 2 );
-//        Product product5 = new Product("древесина", "Китай", 200, 2500, 1 );
-        ArrayList<Product> collection = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            Product product = new Product();
-            Product.setName(scanner.nextLine());
-            Product.setCountry(scanner.nextLine());
-            Product.setWeight(scanner.nextLine());
-            Product.setPrice(scanner.nextLine());
-            Product.setSort(scanner.nextLine());
-            collection.add(product);
+    private static ArrayList<String> searchNameProduct(int sort, List<Product> woodList) {
+        ArrayList<String> nameSearch = new ArrayList<>();
+        int minPrice = woodList.get(0).getPrice();
+        for (Product product : woodList) {
+            if (product.getSort() == sort && product.getPrice() <= minPrice) {
+                minPrice = product.getPrice();
+            }
         }
+            for (Product product : woodList) {
+                if (product.getPrice() == minPrice && product.getSort() == sort) {
+                    nameSearch.add(product.getName());
+                }
+            }
+            return nameSearch;
     }
+
 }
-}
-}
+
 
 
--- src/main/java/org/example/Lesson3/Ex2/Product.java
+++ src/main/java/org/example/Lesson3/Ex2/Product.java
@@ -1,20 +1,5 @@
 package org.example.Lesson3.Ex2;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class Product {
-//private  String name;
-//private  String country;
-//private Integer weight;
-//private Integer price;
-//private Integer sort;
-//
-//}
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,11 +7,11 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Product2 {
-    private String name;
-    private String country;
-    private double weight;
-    private Integer price;
-    private Integer sort;
+public class Product {
+private  String name;
+private  String country;
+private Integer weight;
+private Integer price;
+private Integer sort;
 
 }
